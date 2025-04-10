a60bdc3914f898c544f88a9114039b2359753a95
jerrickho <you@example.com>
2025-04-10 17:37:17 +0800

    yer

--- TheNatureOfScience/MainMenuBottom.js
+++ TheNatureOfScience/MainMenuBottom.js
@@ -13,3 +13,24 @@ function closeModal() {
   const modal = document.getElementById("modal");
   modal.style.display = "none"; // Hide the modal
 }
+
+function showImageModal(imageSrc) {
+  const modal = document.getElementById("image-modal");
+  const modalImage = document.getElementById("modal-image");
+
+  if (modal && modalImage) {
+    modalImage.src = imageSrc; // Set the image source
+    modal.style.display = "flex"; // Show the modal
+  } else {
+    console.error("Modal or modal image element not found.");
+  }
+}
+
+function closeImageModal() {
+  const modal = document.getElementById("image-modal");
+  if (modal) {
+    modal.style.display = "none"; // Hide the modal
+  } else {
+    console.error("Modal element not found.");
+  }
+}
